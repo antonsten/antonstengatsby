091dd0b145e02920bd12ccd5f571c4104eed7793
hansson <alexander.hansson@bulldozer.se>
2019-05-02 13:27:48 +0200

    fix bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,41 +1,13 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
 
 function Bio() {
   return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        return (
-          <div>
-            <p>
-              Välkommen
-            </p>
-          </div>
-        )
-      }}
-    />
+    <div>
+      <p>
+        Välkommen
+      </p>
+    </div>
   )
 }
-
-const bioQuery = graphql`
-  query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-    site {
-      siteMetadata {
-        author
-        social {
-          twitter
-        }
-      }
-    }
-  }
-`
 
 export default Bio
