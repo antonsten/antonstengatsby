d729412b8c773b44cd7f800be5b546480c5f7009
hansson <alexander.hansson@bulldozer.se>
2019-05-02 13:25:27 +0200

    remove loggin from gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,8 +37,6 @@ exports.createPages = ({ graphql, actions }) => {
     posts.forEach((post, index) => {
       const previous = index === posts.length - 1 ? null : posts[index + 1].node
       const next = index === 0 ? null : posts[index - 1].node
-
-      console.log(post.node.frontmatter)
       
       createPage({
         path: post.node.fields.slug,
