ab9a8abafc112a12f685b64f12ab1c98411ecd4a
hansson <alexander.hansson@bulldozer.se>
2019-05-23 11:14:45 +0200

    fix form

--- src/components/signupform.js
+++ src/components/signupform.js
@@ -4,10 +4,12 @@ class Form extends React.Component {
   render() {
 
     return (
-      <form name={ this.props.name ? this.props.name : 'newsletter' } className="mt-12 block w-full bg-blacker rounded-sm p-8 text-white" method="POST" data-netlify="true">
+      <form name={ this.props.name ? this.props.name : 'newsletter' } className="mt-12 block w-full bg-blacker rounded-sm p-8 text-white" method="POST" data-netlify="true" netlify-honeypot="bot-field" netlifyid="mc-embedded-subscribe-form" action="/thankyou/">
         <strong className="text-xs uppercase text-orange tracking-wide">{ this.props.title ? this.props.title : 'Sign up for updates' }</strong>
         <p className="leading-normal mt-4 pr-8">{ this.props.text ? this.props.text : 'Sign up and you’ll get new writing about design in your inbox every two weeks. ' }</p>
-
+        <p class="hidden">
+          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+        </p>
         <div className="w-full mt-6 block">
           <input type="text" placeholder="email" name="email" className="text-xs uppercase rounded-sm font-bold tracking-wide px-4 w-full sm:w-64 mr-4"/>
           <input type="submit" value="Sign up" className="btn mt-4 sm:mt-0"/>
