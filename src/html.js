54c83683197b145d36dbd38164fd538f4886d59a
hansson <alexander.hansson@bulldozer.se>
2019-05-28 14:00:12 +0200

    add preconnect GS

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link rel="preconnect" href="https://data2.gosquared.com"/>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
