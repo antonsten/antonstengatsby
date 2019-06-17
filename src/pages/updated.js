cd81a1b6639e8244057317186dee776745af8341
Anton <anton@lepetitgarcon.com>
2019-06-17 15:17:34 +0200

    Create updated.js

---
+++ src/pages/updated.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import '../css/style.css'
+import Layout from "../components/layout"
+import Testimonial from '../components/testimonial';
+import SEO from "../components/seo"
+
+class Thankyou extends React.Component {
+  render() {
+    const { data } = this.props
+      return (
+        <Layout>
+          <SEO title="Thank you, preferences updated!"/>
+            <h1>Your preferences are updated! <span role="img">🙏</span></h1>
+            <p>
+              Thank you! By telling me what you like, I'm able to better give you the content that you find interesting. Anything specific you're wondering about? <Link to="/contact">Let me know.</Link>
+            </p>
+
+        </Layout>
+      );
+  }
+}
+
+export default Thankyou
