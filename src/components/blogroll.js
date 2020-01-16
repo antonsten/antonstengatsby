3d797339665322ab3978cced2d7b9d8fbb7a690f
alexander.hansson <alexander.hansson@glesys.se>
2020-01-16 08:48:35 +0100

    fix text size

--- src/components/blogroll.js
+++ src/components/blogroll.js
@@ -35,7 +35,7 @@ export default () => (
 
           <div className="sm:flex w-full bg-grey-lightest p-8 sm:p-16 sm:pb-6 text-black">
             <div className="mb-6 w-1/2 sm:w-1/3 text-xs font-sans uppercase font-bold">Books</div>
-            <div className="w-full sm:w-2/3 text-base sm:text-lg font-serif leading-tight">
+            <div className="w-full sm:w-2/3 text-lg font-serif leading-tight">
               <a href="https://www.antonsten.com/books/user-experiences-matter" target="_blank" rel="noopener noreferrer">User Experiences that Matter</a> (2016)<br />
               <a href="https://www.antonsten.com/books/masteringfreelance" target="_blank" rel="noopener noreferrer">Mastering Freelance</a> (2017)
             </div>
@@ -43,7 +43,7 @@ export default () => (
 
           <div className="sm:flex w-full bg-grey-lightest p-8 sm:p-16 sm:pb-6 text-black">
             <div className="mb-6 w-1/2 sm:w-1/3 text-xs font-sans uppercase font-bold">Getting started in UX</div>
-            <div className="w-full sm:w-2/3 text-base sm:text-lg font-serif leading-tight">
+            <div className="w-full sm:w-2/3 text-lg font-serif leading-tight">
               <a href="https://www.antonsten.com/whatsux/">What's a 'User Experience' Anyways?</a><br />
               <a href="https://www.antonsten.com/learn-ux/">How Do You Learn UX?</a><br />
               <a href="https://www.antonsten.com/uxdesigner/">Working as a UX Designer</a>
@@ -52,7 +52,7 @@ export default () => (
 
           <div className="sm:flex w-full bg-grey-lightest p-8 sm:p-16 sm:pb-6 text-black">
             <div className="mb-6 w-1/2 sm:w-1/3 text-xs font-sans uppercase font-bold">Grow within UX</div>
-            <div className="w-full sm:w-2/3 text-base sm:text-lg font-serif leading-tight">
+            <div className="w-full sm:w-2/3 text-lg font-serif leading-tight">
               <a href="https://www.antonsten.com/uxlead/">Working as a UX Lead</a><br />
               <a href="https://www.antonsten.com/uxstrategy/">Defining a UX Strategy</a><br />
               <a href="https://www.antonsten.com/writing-ux/">Writing as Part of the UX Process</a><br />
@@ -61,7 +61,7 @@ export default () => (
 
           <div className="sm:flex w-full bg-grey-lightest p-8 sm:p-16 text-black">
             <div className="mb-6 w-1/2 sm:w-1/3 text-xs font-sans uppercase font-bold">Most popular</div>
-            <div className="w-full sm:w-2/3 text-base sm:text-lg font-serif leading-tight mb-6 sm:mb-12">
+            <div className="w-full sm:w-2/3 text-lg font-serif leading-tight mb-6 sm:mb-12">
               <a href="https://www.antonsten.com/ai-ethics/">AI Ethics - A New Skill for UX-Designers</a><br />
               <a href="https://www.antonsten.com/moral-implications-apps/">Designer Ethics & The Moral Implications of our Apps</a><br />
               <a href="https://www.antonsten.com/future-ux-designer/">The Future of the UX-Designer</a><br />
@@ -81,7 +81,7 @@ export default () => (
       <div className="w-1/2 sm:w-1/3">
       </div>
       <div className="w-full sm:w-2/3">
-        <Link className="font-serif leading-tight text-black" to="/newsletter">
+        <Link className="font-serif text-lg leading-tight text-black" to="/newsletter">
           Subscribers get these posts first. Sign up!
         </Link>
       </div>
@@ -96,7 +96,7 @@ export default () => (
                 <span className="hidden sm:block text-xs uppercase font-sans tracking-wide text-grey">{node.frontmatter.date}</span>
               </div>
               <div className="w-full sm:w-2/3">
-                <Link className="relative font-serif leading-tight no-underline text-black" to={node.fields.slug}>
+                <Link className="relative text-lg font-serif leading-tight no-underline text-black" to={node.fields.slug}>
                   {title}
                 </Link>
               </div>
