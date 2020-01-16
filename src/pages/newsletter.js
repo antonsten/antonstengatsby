4b1505596d42bd4b25908e8cd161c514c4774d12
alexander.hansson <alexander.hansson@glesys.se>
2020-01-16 09:44:59 +0100

    fixes newsletter

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -21,8 +21,10 @@ class Contact extends React.Component {
             </div>
           </div>
           <div className="sm:-mx-16">
-            <div className="sm:flex w-full mb-16 bg-black pr-8 text-white">
-              <Form naked="true"/>
+            <div className="sm:flex w-full mb-16 bg-black text-white">
+              <div className="px-6 sm:px-12 flex w-full">
+                <Form naked="true"/>
+              </div>
             </div>
           </div>
           <Testimonial logo="/images/clients/winegallery.png" text="Great laser focused UX content, told in an easy to understand way, helping to make sure I keep my eye on the UX ball. Don’t miss out." author="Tom Walenkamp, The Wine Gallery"/>
