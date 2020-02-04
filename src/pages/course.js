df862fac01e5d25ad41f0af3234849dbc7caa1b7
Anton <anton@lepetitgarcon.com>
2020-02-04 12:45:21 +0100

    blog + course

--- src/pages/course.js
+++ src/pages/course.js
@@ -8,12 +8,12 @@ class Contact extends React.Component {
   render() {
       return (
         <Layout>
-          <SEO title="UX Design Class"/>
+          <SEO title="Pick my brain!"/>
 
           <div className="sm:-mx-16">
             <div className="sm:flex w-full mt-16 p-8 sm:p-16 text-black">
               <div className="w-1/2 sm:w-1/3"></div>
-              <div className="w-full sm:w-2/3"><h1>Design Class</h1>
+              <div className="w-full sm:w-2/3"><h1>Pick my brain!</h1>
               <del className="mb-8">
               Growing as a designer is difficult. Most of us grow only through years and years of 'work experience' and while we want to grow and evolve as designers faster things get in the way. There's never the perfect time. There are no courses available or they cost a trillion dollar. </del>
               </div>
@@ -26,43 +26,24 @@ class Contact extends React.Component {
               <div className="w-1/2 sm:w-1/3"></div>
               <div className="w-full sm:w-2/3">
                   <del className="mb-8">
-                  After teaching at <a href="https://www.hyperisland.com/">Hyper Island</a> for nearly two decades, I wanted to bring the highlights of those experiences into my own easily-accessible, affordable course.
-                  <ul>
-                  <li>It's not a UX-course, but I will talk a great bit about how to create great user experiences.</li>
-                  <li>It's not a course on presentation skills, but I'll teach you how to communicate more clearly.</li>
-                  <li>It's not a business course, but I'll make sure you talk about design in a way that speaks to stake holders.</li>
-                  <li>It's not a career course, but we'll identify where you want to go in your career.</li>
-                  <li>And most importantly, it's a course with a curriculum that's flexible according to what you need, where you are, and who you are.</li></ul>
+                  After teaching at Hyper Island for nearly two decades, I wanted to share the experience with more people. I wanted to be able to give expert advice to designers and teams while lowering the barrier to get started. <br /><br /><strong>Introducing Pick My Brain! </strong>
                   </del>
               </div>
               </div>
             </div>
 
-<div className="w-full flex">
-  <div className="sm:-mx-16">
-    <div className="sm:flex w-full bg-black p-8 sm:p-16 text-white">
-      <div className="w-1/2 sm:w-1/3"></div>
-      <div className="w-full sm:w-2/3">
-        <h2 className="leading-tight">Classes</h2>
-        <p>We'll meet online every Tuesday (Thursdays for 1-1's) for 5 weeks. Nothing is pre-recorded —· everything is done in real time to cater to your exact needs. We'll be a small group (first class is limited to 6 people) so I can focus on each and everyone.</p>
-      </div>
-    </div>
-  </div>
-</div>
-
 <div className="sm:-mx-16">
   <div className="sm:flex w-full mt-16 mb-16 bg-grey-lightest p-8 sm:p-16 text-black">
     <div className="w-1/2 sm:w-1/3"></div>
     <div className="w-full sm:w-2/3">
-      <h2 className="leading-tight">Topics</h2>
-      <p>Over the course of 5 weeks you'll learn how to:<br /><br />
+      <p>We'll meet online through Zoom - you get to pick a time that works well with your schedule. We can discuss a variety of topics, here are a couple of things where I believe you'll be able to get the most out of me: <br /><br />
       <span class="font-sans font-bold text-sm uppercase">Identify what role you want to have in a team</span><br />
       I've worked my way from design intern to Creative Director. From a freelance designer to a UX Team Leader. I know there is no role that is sunshine at all times, but I know you'll need to identify the role you want to be in order to enjoy those rainy days too. I will share my experiences of working in different roles and what I found were the upsides and downsides to all of them. Once you know what your goal is, I'll help you set a path to get you there. For groups, this can be used as an exercise for you to rethink your current setup or structure.<br /><br />
       <span class="font-sans font-bold text-sm uppercase">Talk and sell design inside your company</span><br />
       During the last two decades, I've worked with everything from one-man teams to Forbes 100 companies, from heavily design-oriented companies to technical process-oriented companies. Through each and every company and project, I've learned that there is no golden rule that works everywhere. I will help you find your voice within your company to make you more confident in talking about - and selling - design internally. How do you make sure you are the one presenting your design? And even more importantly, how do you feel confident doing it? I will share the techniques I use when presenting design that will get your project approved.<br /><br />
       <span class="font-sans font-bold text-sm uppercase">Structure projects like a professional</span><br />
       Once your project is approved, how do you set it up to work as efficiently as possible especially when you run 5 or 6 projects at once? I will share my techniques for running multiple projects at the same time, what tools I use, how I have them set up, and how I schedule my time!<br /><br />
-      <span class="font-sans font-bold text-sm uppercase">Per request / Optional: One-to-one's!</span><br /> You'll get private access to me where you can discuss whatever you want to. These discussions are NDA-covered, so you can discuss real-life scenarios that you're working with or keep it on a more general level. Either way, a great way to have more in-depth discussions leading to solutions catered to your needs.<br /><br />
+      <span class="font-sans font-bold text-sm uppercase">Stuck in a project?</span><br /> If you feel stuck in a project or with a specific feature, we'll walk through during an hour and I'll offer valuable help to help you see things more clearly. Sometimes an outside view on things is all that's needed! <br /><br />
       </p>
     </div>
   </div>
@@ -73,59 +54,35 @@ class Contact extends React.Component {
     <div className="w-1/2 sm:w-1/3"></div>
     <div className="w-full sm:w-2/3">
       <h2 className="leading-tight">Who's this for?</h2>
-      <p>You're already a designer and, most likely, have worked as one for many years. Perhaps you're the only designer internally at your company and want to understand how to grow to something more. Maybe you're even on the verge of hiring your second designer and unsure about what that will mean for you. Perhaps you're a designer within a company full of designers, but are finding it difficult to see what's next and how to communicate your need to grow. You could be a team that wants to find better ways of working together as well as within the company. Any of these situations - and more - I'll be able to help you find your next steps.</p>
+      <p>You're already a designer and, most likely, have worked as one for many years. Perhaps you're the only designer internally at your company and want to understand how to grow to something more. Maybe you're even on the verge of hiring your second designer and unsure about what that will mean for you. Perhaps you're a designer within a company full of designers but are finding it difficult to see what's next and how to communicate your needs to grow. You could be a team that wants to find better ways of working together as well as within the company. Any of these situations - and more - I'll be able to help you find your next steps.</p>
     </div>
   </div>
 </div>
 
-<div className="sm:-mx-16">
-  <div className="sm:flex w-full mt-16 mb-16 bg-grey-lightest p-8 sm:p-16 text-black">
-    <div className="w-1/2 sm:w-1/3"></div>
-    <div className="w-full sm:w-2/3">
-      <h2 className="leading-tight">Everything you need to know</h2>
-      <p><span class="font-sans font-bold text-sm uppercase">Online classes:</span><br/> 10 hours in total including private one-to-one's. Everything is in real time so there's always an opportunity to ask questions, comment, and disagree (great conversations can come from disagreement!).</p>
-      <p><span class="font-sans font-bold text-sm uppercase">Personal studies:</span><br/> You will need to spend time to reflect, analyze, and ACT in-between sessions. This is of course entirely up to you and you can't fail this class either way. But the more you invest, the more you'll get out of it. There's no home work!</p>
-      <p><span class="font-sans font-bold text-sm uppercase">Dates:</span><br/> Between February 25 - March 31.</p>
-      <p><span class="font-sans font-bold text-sm uppercase">Times:</span><br/> Depending on the class participants I'll be flexible to what works for most of you, but expect 2 hours in the afternoon (CET) / in the late morning (EST) / in the early morning (PST).</p>
-      <p><span class="font-sans font-bold text-sm uppercase">Location:</span><br/> Online, Zoom video call</p>
-      <p><span class="font-sans font-bold text-sm uppercase">Teacher:</span><br/> Anton Sten, UX Lead. See <a href="http://linkedin.com/in/antonsten/">Linkedin</a> for full resume + secret guest appearances</p>
-
-    </div>
-  </div>
-</div>
 
   <div className="sm:-mx-16">
   <div className="sm:flex w-full p-8 sm:p-16 text-black">
   <div className="w-1/2 sm:w-1/3"></div>
-  <div className="w-full sm:w-2/3"><h2 className="leading-tight">Enrollment options</h2>
+  <div className="w-full sm:w-2/3"><h2 className="leading-tight">Cost</h2>
 <br /><br />
-  <span class="font-sans font-bold text-sm uppercase">Classes only (5 x 2hr) - $750/person</span><br />
-<p className="leading-tight">Join the classes and discuss with the other students. Ask questions in real time.<br /><br />
-$750 / person (limited to 6 spots - <em>only 3 left</em>)</p></div></div></div>
+  <span class="font-sans font-bold text-sm uppercase">One single hour (1 hr)</span><br />
+<p className="leading-tight">One hour where I'll promise you we'll get through a lot. This could be about your career or a specific product feature. <br /><br />
+$300 / hour</p></div></div></div>
 
 <div className="sm:-mx-16">
 <div className="sm:flex w-full border-t-8 p-8 sm:p-16 text-black">
 <div className="w-1/2 sm:w-1/3"></div>
-<div className="w-full sm:w-2/3"><span class="font-sans font-bold text-sm uppercase">Classes + One-on-one sessions
-(5 x 2hr) + (5 x 1hr) - $1500/person</span><br />
-<p className="leading-tight">Join the classes and discuss with the other students. Ask questions in real time. Get 5 personal calls where we can discuss anything you want!<br /><br />
-$1500 / person (limited to 6 spots - <em>only 3 left</em>)</p></div></div>
+<div className="w-full sm:w-2/3"><span class="font-sans font-bold text-sm uppercase">One hour - 6 pack! (1 hr x 6)</span><br />
+<p className="leading-tight">Like the one hour sessions but want to make it recurring? Great, let's schedule for one hour every week. This way we'll get through even more and you get one hour for free.<br /><br />
+$1500 / 6 hours</p></div></div>
 </div>
-
-<div className="sm:-mx-16">
-<div className="sm:flex w-full border-t-8 p-8 sm:p-16 text-black">
-<div className="w-1/2 sm:w-1/3"></div>
-<div className="w-full sm:w-2/3"><span class="font-sans font-bold text-sm uppercase">Classes - for groups! (5 x 2hr) - $600/Person</span><br />
-<p className="leading-tight">Same curriculum but tailored to your team! Up to 6 people in one team and we'll discuss the topics from your perspective.<br /><br />
-Want a customised curriculum? We can make that work! <a href="mailto:anton@antonsten.com">Get in touch!</a></p></div>
-</div></div>
 
 <div className="sm:-mx-16">
 <div className="sm:flex w-full mb-16 bg-black p-8 sm:p-16 text-white">
 <div className="w-1/2 sm:w-1/3"></div>
-<div className="w-full sm:w-2/3"><span class="font-sans font-bold text-sm uppercase">Enrollment</span><br />
+<div className="w-full sm:w-2/3"><span class="font-sans font-bold text-sm uppercase">Schedule</span><br />
 <p className="leading-tight">Are you interested? Great! Please fill out the form and I'll get back to you with more information!</p>
-<a class="btn" href="https://antonsten.typeform.com/to/j669Fg">Sign up</a></div>
+<a class="btn" href="https://antonsten.typeform.com/to/j669Fg">Schedule</a></div>
 </div>
 </div>
 
