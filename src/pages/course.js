80968ed16a8548359ea48da7e76d5db80c219dd0
Anton <anton@lepetitgarcon.com>
2020-02-05 08:35:49 +0100

    Update course.js

--- src/pages/course.js
+++ src/pages/course.js
@@ -15,7 +15,7 @@ class Contact extends React.Component {
               <div className="w-1/2 sm:w-1/3"></div>
               <div className="w-full sm:w-2/3"><h1>Pick my brain!</h1>
               <del className="mb-8">
-              Growing as a designer is difficult. Most of us grow only through years and years of 'work experience' and while we want to grow and evolve as designers faster things get in the way. There's never the perfect time. There are no courses available or they cost a trillion dollar. </del>
+              Growing as a designer is difficult. Most of us grow only through years and years of 'work experience' and while we want to grow and evolve as designers faster things get in the way. There's never the perfect time. There are no courses available or they cost a trillion dollar.</del>
               </div>
             </div>
           </div>
@@ -26,7 +26,7 @@ class Contact extends React.Component {
               <div className="w-1/2 sm:w-1/3"></div>
               <div className="w-full sm:w-2/3">
                   <del className="mb-8">
-                  After teaching at Hyper Island for nearly two decades, I wanted to share the experience with more people. I wanted to be able to give expert advice to designers and teams while lowering the barrier to get started. <br /><br /><strong>Introducing Pick My Brain! </strong>
+                  After teaching at Hyper Island for nearly two decades, I wanted to share the experience with more people. I wanted to be able to give advice to designers and teams while lowering the barrier to getting started.<br /><strong>Introducing Pick My Brain! </strong>
                   </del>
               </div>
               </div>
@@ -36,14 +36,14 @@ class Contact extends React.Component {
   <div className="sm:flex w-full mt-16 mb-16 bg-grey-lightest p-8 sm:p-16 text-black">
     <div className="w-1/2 sm:w-1/3"></div>
     <div className="w-full sm:w-2/3">
-      <p>We'll meet online through Zoom - you get to pick a time that works well with your schedule. We can discuss a variety of topics, here are a couple of things where I believe you'll be able to get the most out of me: <br /><br />
+      <p>We'll meet online through Zoom - you get to pick times that work well with your schedule. We can discuss a variety of topics, including, but not limited to: <br /><br />
       <span class="font-sans font-bold text-sm uppercase">Identify what role you want to have in a team</span><br />
-      I've worked my way from design intern to Creative Director. From a freelance designer to a UX Team Leader. I know there is no role that is sunshine at all times, but I know you'll need to identify the role you want to be in order to enjoy those rainy days too. I will share my experiences of working in different roles and what I found were the upsides and downsides to all of them. Once you know what your goal is, I'll help you set a path to get you there. For groups, this can be used as an exercise for you to rethink your current setup or structure.<br /><br />
+      I've worked my way from design intern to Creative Director. From a freelance designer to a UX Team Leader. I know there is no role that is sunshine at all times, but do know you'll need to identify the role you want to be in order to enjoy those rainy days too. I will share my experiences of working in different roles with you and discuss what I found were the upsides and downsides to all of them. Once we determine what your goal is, I'll help you set a path to get you there. For group sessions, this can be used as an exercise to rethink your current setup/structure.<br /><br />
       <span class="font-sans font-bold text-sm uppercase">Talk and sell design inside your company</span><br />
-      During the last two decades, I've worked with everything from one-man teams to Forbes 100 companies, from heavily design-oriented companies to technical process-oriented companies. Through each and every company and project, I've learned that there is no golden rule that works everywhere. I will help you find your voice within your company to make you more confident in talking about - and selling - design internally. How do you make sure you are the one presenting your design? And even more importantly, how do you feel confident doing it? I will share the techniques I use when presenting design that will get your project approved.<br /><br />
+      During the last two decades, I've worked with everything from one-man teams to Forbes 100 companies, from heavily design-oriented companies to technical process-oriented companies. Through each and every company and project, I've learned that there is no golden rule that works everywhere. I will help you find your voice within your company to make you more confident in talking about - and selling - design internally. How do you make sure you are the one presenting your design? And even more importantly, how can you feel more confident doing it? I will share the techniques I use when presenting a design that will get your project approved. <br /><br />
       <span class="font-sans font-bold text-sm uppercase">Structure projects like a professional</span><br />
-      Once your project is approved, how do you set it up to work as efficiently as possible especially when you run 5 or 6 projects at once? I will share my techniques for running multiple projects at the same time, what tools I use, how I have them set up, and how I schedule my time!<br /><br />
-      <span class="font-sans font-bold text-sm uppercase">Stuck in a project?</span><br /> If you feel stuck in a project or with a specific feature, we'll walk through during an hour and I'll offer valuable help to help you see things more clearly. Sometimes an outside view on things is all that's needed! <br /><br />
+      Once your project is approved, how do you set it up to work as efficiently as possible especially when you are stuck running 5 or 6 projects at the same time? I will share my techniques for running multiple projects at the same time, what tools I use, how I have them set up, and how I schedule my time! <br /><br />
+      <span class="font-sans font-bold text-sm uppercase">Stuck in a project?</span><br />If you feel like you're stuck in a project or with a specific feature, we'll walk through it and I'll offer valuable insight to help you see things more clearly. Sometimes an outside view on things is all you need! <br /><br />
       </p>
     </div>
   </div>
@@ -54,7 +54,7 @@ class Contact extends React.Component {
     <div className="w-1/2 sm:w-1/3"></div>
     <div className="w-full sm:w-2/3">
       <h2 className="leading-tight">Who's this for?</h2>
-      <p>You're already a designer and, most likely, have worked as one for many years. Perhaps you're the only designer internally at your company and want to understand how to grow to something more. Maybe you're even on the verge of hiring your second designer and unsure about what that will mean for you. Perhaps you're a designer within a company full of designers but are finding it difficult to see what's next and how to communicate your needs to grow. You could be a team that wants to find better ways of working together as well as within the company. Any of these situations - and more - I'll be able to help you find your next steps.</p>
+      <p>You're already a designer and, most likely, have worked as one for many years. Perhaps you're the only designer internally at your company and want to understand how to grow to something more. Maybe you're even on the verge of hiring your second designer and unsure about what that will mean for you. Perhaps you're a designer within a company full of designers, but are finding it difficult to see what's next and how to communicate your needs to grow. You could even be a team that wants to find better ways of working together as well as within the company. Any of these situations - and more - I'll be able to help you find your next steps.</p>
     </div>
   </div>
 </div>
@@ -66,15 +66,15 @@ class Contact extends React.Component {
   <div className="w-full sm:w-2/3"><h2 className="leading-tight">Cost</h2>
 <br /><br />
   <span class="font-sans font-bold text-sm uppercase">One single hour (1 hr)</span><br />
-<p className="leading-tight">One hour where I'll promise you we'll get through a lot. This could be about your career or a specific product feature. <br /><br />
-$300 / hour</p></div></div></div>
+<p className="leading-tight">A lot can be accomplished in one hour. We can work through topics such as your career, a project, or even something as specific as a product feature.<br /><br />
+$350 / hour</p></div></div></div>
 
 <div className="sm:-mx-16">
 <div className="sm:flex w-full border-t-8 p-8 sm:p-16 text-black">
 <div className="w-1/2 sm:w-1/3"></div>
-<div className="w-full sm:w-2/3"><span class="font-sans font-bold text-sm uppercase">One hour - 6 pack! (1 hr x 6)</span><br />
-<p className="leading-tight">Like the one hour sessions but want to make it recurring? Great, let's schedule for one hour every week. This way we'll get through even more and you get one hour for free.<br /><br />
-$1500 / 6 hours</p></div></div>
+<div className="w-full sm:w-2/3"><span class="font-sans font-bold text-sm uppercase">One hour - 5 pack! (1 hr x 5)</span><br />
+<p className="leading-tight">Like the one hour sessions, but want to make it recurring? Great, let's schedule for one hour every other week. This way we'll get through more at a more affordable rate.<br /><br />
+$1500 / 5 hours</p></div></div>
 </div>
 
 <div className="sm:-mx-16">
