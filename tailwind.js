fe3d90256b2067171c2fc0f56702e72971b2afca
Anton Sten <anton@lepetitgarcon.com>
2020-03-30 12:07:01 +0200

    Update tailwind.js

--- tailwind.js
+++ tailwind.js
@@ -245,7 +245,7 @@ module.exports = {
     '2xl': '1.75rem',    // 28px
     '3xl': '2rem',  // 30px
     '4xl': '2.75rem',   // 36px
-    '5xl': '4.25rem',      // 56px
+    '5xl': '3.5rem',      // 56px
   },
 
 
