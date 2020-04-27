f7c4aecd0233750b773c0d11b2e922b4247012de
Anton <anton@lepetitgarcon.com>
2020-04-27 09:04:33 +0200

    Update tailwind.js

--- tailwind.js
+++ tailwind.js
@@ -239,7 +239,7 @@ module.exports = {
   textSizes: {
     'xs': '12px',     // 11px
     'sm': '.8rem',    // 14px
-    'base': '1rem',     // 16px
+    'base': '1.05rem',     // 16px
     'lg': '1.2rem',   // 20px
     'xl': '1.6rem',    // 24px
     '2xl': '1.75rem',    // 28px
